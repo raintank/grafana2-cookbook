5f840d5254f5836df4d08cf899f591cb789f2524
ctdk <jbingham@gmail.com>
2015-05-22 10:48:15 -0700

    New configuration options for grafana

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,15 +9,25 @@ default['grafana']['group'] = 'grafana'
 default['grafana']['conf_ini'] = '/etc/grafana/grafana.ini'
 default['grafana']['repo_url'] = 'https://packagecloud.io'
 default['grafana']['version'] = '' # '2.0.2-1' If you really want to pin
+default['grafana']['ssl_cert_file'] = '/etc/grafana/ssl.crt'
+default['grafana']['ssl_key_file'] = '/etc/grafana/ssl.key'
+default['grafana']['ssl_cert_content'] = nil
+default['grafana']['ssl_key_content'] = nil
 
 ### PATHS ###
 default['grafana']['log_dir'] = '/var/log/grafana'
+default['grafana']['data_dir'] = '/var/lib/grafana'
+default['grafana']['static_root'] = 'public'
 
 ### SERVER ###
 
 default['grafana']['port'] = '3000'
 default['grafana']['protocol'] = 'http'
 default['grafana']['domain'] = 'grafana2.example.com'
+default['grafana']['app_mode'] = 'production'
+default['grafana']['root_url'] = '%(protocol)s://%(domain)s:%(http_port)s/'
+default['grafana']['router_logging'] = false
+default['grafana']['enable_gzip'] = false
 
 ### DATABASE ###
 ## mysql
@@ -41,10 +51,44 @@ default['grafana']['db_name'] = 'grafana'
 ### AUTH.ANONYMOUS ###
 default['grafana']['anon_enabled'] = 'true' # 'true' or 'false'
 default['grafana']['org_name'] = 'Main Org.'
+default['grafana']['org_role'] = 'Viewer'
 
 ### SESSION ###
 default['grafana']['session_type'] = 'memory' # Either 'memory', 'file', 'mysql', or 'postgres'
 # default['grafana']['session_type'] = 'mysql' # Either 'memory', 'file', 'mysql', or 'postgres'
+default['grafana']['cookie_name'] = 'grafana_sess'
+default['grafana']['cookie_secure'] = false
+default['grafana']['session_life_time'] = 84600
 
 ### LOGGING ###
 default['grafana']['log_level'] = 'Info' # Either "Trace", "Debug", "Info", "Warn", "Error", "Critical"
+
+### REPORTING ###
+default['grafana']['reporting_enabled'] = true
+
+### SECURITY ###
+default['grafana']['admin_user'] = 'admin'
+default['grafana']['admin_password'] = 'admin'
+default['grafana']['secret_key'] = 'AAAABBBBBCCCC'
+default['grafana']['login_remember_days'] = 7
+default['grafana']['cookie_username'] = 'grafana_user'
+default['grafana']['cookie_remember_name'] = 'grafana_remember'
+
+### USERS ###
+default['grafana']['allow_sign_up'] = true
+default['grafana']['allow_org_create'] = true
+default['grafana']['auto_assign_org'] = true
+default['grafana']['auto_assign_org_role'] = node['grafana']['org_role']
+
+### EVENT PUBLISHING
+default['grafana']['event_publish'] = true
+default['grafana']['rabbitmq_host'] = 'localhost'
+default['grafana']['event_exchange'] = 'grafana_events'
+
+### RAINTANK ###
+default['grafana']['raintank'] = true
+default['grafana']['graphite_host'] = 'localhost'
+default['grafana']['graphite_port'] = 8888
+default['grafana']['elasticsearch_host'] = 'localhost'
+# TODO: there *has* to be a better way to specify this path.
+default['grafana']['litmus_path'] = '/go/src/github.com/grafana/grafana/public/plugins/raintank/dashboards/litmus'
--- recipes/config.rb
+++ recipes/config.rb
@@ -1,7 +1,32 @@
+cert_file = cert_key = nil
+
+if node['grafana']['ssl_cert_content'] && node['grafana']['ssl_key_content']
+  cert_file = node['grafana']['ssl_cert_file']
+  cert_key = node['grafana']['ssl_key_file']
+  file node['grafana']['ssl_cert_file'] do
+    owner node['grafana']['user']
+    group node['grafana']['group']
+    mode '0600'
+    content node['grafana']['ssl_cert_content']
+    action :create
+  end
+  file node['grafana']['ssl_key_file'] do
+    owner node['grafana']['user']
+    group node['grafana']['group']
+    mode '0600'
+    content node['grafana']['ssl_key_content']
+    action :create
+  end
+end
+
 template node['grafana']['conf_ini'] do
   source 'grafana.ini.erb'
   mode '600'
   owner node['grafana']['user']
   group node['grafana']['group']
+  variables({
+    cert_file: cert_file,
+    cert_key: cert_key
+  })
   notifies :restart, 'service[grafana-server]', :delayed
 end
